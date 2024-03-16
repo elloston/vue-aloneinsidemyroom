5df2b46b9df43df03ad241bd1f037cb4baf8b97b
elloston <elloston@icloud.com>
2024-03-16 12:30:12 +0700

    Changed theme

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -23,7 +23,7 @@ const myCustomLightTheme: ThemeDefinition = {
 
 export default createVuetify({
   theme: {
-    defaultTheme: "dark",
+    defaultTheme: "light",
     themes: {
       myCustomLightTheme,
     },
